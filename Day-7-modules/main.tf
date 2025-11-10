37920657a1bd8e1ccfda5e080f2c4221f3fbfb72
Vinayak <vinayakmohitecse@gmail.com>
2025-11-10 16:02:35 +0530

    day -10

--- Day-7-modules/main.tf
+++ Day-7-modules/main.tf
@@ -1,7 +1,27 @@
 
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_cidr
+}
+
+
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2_sg"
+  vpc_id      = aws_vpc.main.id
+
+}
+
 resource "aws_instance" "name" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.public_subnet.id
+  vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
+  availability_zone       = "us-east-1a" 
+
   tags = {
     Name = "Terraform-EC2"
   }
--- Day-7-modules/variables.tf
+++ Day-7-modules/variables.tf
@@ -1,3 +1,15 @@
+
+# VPC CIDR
+variable "vpc_cidr" {
+  type        = string
+  default     = ""
+}
+
+# Subnet CIDR
+variable "subnet_cidr" {
+  type        = string
+  default     = ""
+}
 # EC2 AMI ID
 variable "ami_id" {
   type        = string
